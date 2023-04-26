87bf53d0c3600b10d65dafc49066bd5cdbcb61cb
IC-KZM <123092306+IC-KZM@users.noreply.github.com>
2023-04-27 06:45:02 +0700

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -220,23 +220,39 @@
 	/*
 	* Konami code
 	*/
-	function onKonamiCode(cb) {
-		var input = '';
-		var key = '38384040373937396665';
-		document.addEventListener('keydown', function (e) {
-		  input += ("" + e.keyCode);
-		  if (input === key) {
-			return cb();
-		  }
-		  if (!key.indexOf(input)) return;
-		  input = ("" + e.keyCode);
-		});
-	}
-
-	onKonamiCode(function () {		
-	  	alert('Ah! You remembered!')
-	  	window.location.href = "assets/video/a.mp4";
-	})
+// Cheat Codes
+neededkeys = [38,38,40,40,37,39,37,39,66,65], started = false, count = 0;
+$(document).keydown(function(e) {
+    key = e.keyCode;
+    if (!started) {
+        if (key == 38) {
+            started = true;
+        }
+    }
+    if (started) {
+        if (neededkeys[count] == key) {
+            count++;
+        } else {
+            reset();
+        }
+        if (count == 10) {
+            reset();
+            // Do your stuff here
+            alert('Cheat Codes Activated');
+            $('body').css('background-color', '#FFA8A8');
+            // Turn down for what
+            var s=document.createElement('script');
+            s.setAttribute('src','https://nthitz.github.io/turndownforwhatjs/tdfw.js');
+            document.body.appendChild(s);
+        }
+    } else {
+        reset();
+    }
+});
+function reset() {
+    started = false;
+    count = 0;
+}
 	  
 })()
 
